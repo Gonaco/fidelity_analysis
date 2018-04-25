203268c6bfd763f5b555096ae0743a5508031ceb
Gonaco <danielmoremanza@gmail.com>
2018-04-25 21:59:46 +0200

    qubit state readout in the begining?

--- toffoli_simulation.py
+++ toffoli_simulation.py
@@ -8,6 +8,11 @@ qx.set("test_output/toffoli_gate.qasm")
 N_exp = 1000
 
 for i in range(N_exp):
+
+    c0 = qx.get_measurement_outcome(0)
+    c1 = qx.get_measurement_outcome(1)
+    c2 = qx.get_measurement_outcome(2)
+
     qx.execute()                            # execute
 
     c0 = qx.get_measurement_outcome(0)
