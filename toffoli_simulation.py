37332e5e7fd439995251992764ac5e7ca3e9520b
Gonaco <danielmoremanza@gmail.com>
2018-04-28 18:42:06 +0200

    testing different states

--- toffoli_simulation.py
+++ toffoli_simulation.py
@@ -37,7 +37,8 @@ for i in range(N_exp):
     print("{} {} {}\n".format(c0, c1, c2))
 
     sigma_states = np.array([c0, c1, c2], dtype=int)
-    state_n = int("".join(sigma_states), 2)
-    sigma = qt.fock(d_hilbert_spc, state_n)
-    f = qt.metrics.fidelity(rho, sigma)
-    print(f)
+    print("".join(sigma_states))
+    # state_n = int("".join(sigma_states), 2)
+    # sigma = qt.fock(d_hilbert_spc, state_n)
+    # f = qt.metrics.fidelity(rho, sigma)
+    # print(f)
