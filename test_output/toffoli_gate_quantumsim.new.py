7639c71fba49b4362c271a377342f36e2dcbeae9
Gonaco <danielmoremanza@gmail.com>
2018-05-05 22:59:07 +0200

    Fidelity?

--- test_output/toffoli_gate_quantumsim.new.py
+++ test_output/toffoli_gate_quantumsim.new.py
@@ -74,7 +74,7 @@ c_clean = toffoli_gate_decomposition_circuit()
 state_clean = sparsedm.SparseDM(c_clean.get_qubit_names())
 c_clean.apply_to(state_clean)
 
-state_decay = quantumsim.sparsedm.SparseDM(c.get_qubit_names())
+state_decay = sparsedm.SparseDM(c.get_qubit_names())
 c.apply_to(state_decay)
 
 print("Bell state fidelity: ", np.dot(
