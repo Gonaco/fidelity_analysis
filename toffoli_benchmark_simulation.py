279c5169472272707f11c56bac35b39a4b5046a9
Gonaco <danielmoremanza@gmail.com>
2018-05-22 22:49:12 +0200

    Experimets results

--- toffoli_benchmark_simulation.py
+++ toffoli_benchmark_simulation.py
@@ -32,7 +32,7 @@ def analysis():
 
         fidelity_registry.append(fidelity(expected_q_state, q_state))
 
-        if fidelity_registry[i] - success_registry[i] not = 0:
+        if fidelity_registry[i] - success_registry[i] != 0:
             input("Fidelity and Success not equal")
 
     print(probability_of_success(success_registry, N_exp))
