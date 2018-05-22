d014b0635cca74d1a1a061e9fc10fe88f1076bec
Gonaco <danielmoremanza@gmail.com>
2018-05-22 22:36:06 +0200

    Experimets results

--- toffoli_benchmark_simulation.py
+++ toffoli_benchmark_simulation.py
@@ -34,6 +34,7 @@ def analysis():
 
     print(probability_of_success(success_registry, N_exp))
     print(fidelity_registry)
+    print(np.mean(fidelity_registry))
 
 
 def output_quantum_state(q_state, N_qubits):
